db2f39fb9275690a6d1d22564deda8ba9a3c44ec
JacBac <jacbachelier@gmail.com>
2013-10-05 13:45:44 +0200

    add doctrine extension bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             
             new Anneaux\PortailBundle\PortailBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
